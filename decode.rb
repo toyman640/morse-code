6bec85b9a63ecd1d38bfc9e97ee5ece11b1415c3
gisa chris <gisachris24@gmail.com>
2023-07-27 12:35:20 -0700

    fix linter errors

--- decode.rb
+++ decode.rb
@@ -1,143 +1,67 @@
+MORSE_TO_CHAR = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z',
+  '-----' => '0',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9',
+  '.-.-.-' => '.',
+  '--..--' => ',',
+  '..--..' => '?',
+  '-.-.--' => '!',
+  '-....-' => '-',
+  '-.--.' => '(',
+  '-.--.-' => ')',
+  '.-..-.' => '"',
+  '.----.' => "'",
+  '-..-.' => '/',
+  '.-.-.' => '+',
+  '---...' => ':',
+  '-.-.-.' => ';',
+  '..--.-' => '_',
+  '...-..-' => '$'
+}.freeze
+
 def decode_char(morse_code)
-    case morse_code
-    when '.-'
-      'A'
-    when '-...'
-      'B'
-    when '-.-.'
-      'C'
-    when '-..'
-      'D'
-    when '.'
-      'E'
-    when '..-.'
-      'F'
-    when '--.'
-      'G'
-    when '....'
-      'H'
-    when '..'
-      'I'
-    when '.---'
-      'J'
-    when '-.-'
-      'K'
-    when '.-..'
-      'L'
-    when '--'
-      'M'
-    when '-.'
-      'N'
-    when '---'
-      'O'
-    when '.--.'
-      'P'
-    when '--.-'
-      'Q'
-    when '.-.'
-      'R'
-    when '...'
-      'S'
-    when '-'
-      'T'
-    when '..-'
-      'U'
-    when '...-'
-      'V'
-    when '.--'
-      'W'
-    when '-..-'
-      'X'
-    when '-.--'
-      'Y'
-    when '--..'
-      'Z'
-    when '-----'
-      '0'
-    when '.----'
-      '1'
-    when '..---'
-      '2'
-    when '...--'
-      '3'
-    when '....-'
-      '4'
-    when '.....'
-      '5'
-    when '-....'
-      '6'
-    when '--...'
-      '7'
-    when '---..'
-      '8'
-    when '----.'
-      '9'
-    when '.-.-.-'
-      '.'
-    when '--..--'
-      ','
-    when '..--..'
-      '?'
-    when '-.-.--'
-      '!'
-    when '-....-'
-      '-'
-    when '-.--.'
-      '('
-    when '-.--.-'
-      ')'
-    when '.-..-.'
-      '"'
-    when '.----.'
-      "'"
-    when '-..-.'
-      '/'
-    when '.-.-.'
-      '+'
-    when '---...'
-      ':'
-    when '-.-.-.'
-      ';'
-    when '.-...'
-      '&'
-    when '.-..-'
-      '"'
-    when '-.--.'
-      ')'
-    when '..--.-'
-      '_'
-    when '...-..-'
-      '$'
-    when '.-.-.--'
-      '!'
-    when '.--.-.'
-      '@'
-    else
-      'Not a Morse code character!'
-    end
-  end
-  
-  def decode_word(morse_word)
-    uncoded_word = ''
-    word_clean_up = morse_word.strip
-    char_array = word_clean_up.split(' ')
-    char_array.each do |char|
-      char_at = decode_char(char)
-      uncoded_word += char_at
-    end
-    uncoded_word
-  end
-  
-  def decode(morse_mssg)
-    uncoded_mssg = ''
-    mssg_clean = morse_mssg.strip
-    word_array = mssg_clean.split('   ')
-    word_array.each do |word|
-      word_final = decode_word(word)
-      uncoded_mssg += ' ' + word_final
-    end
-    uncoded_mssg.strip
-  end
-  
-  print decode('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
-  
+  MORSE_TO_CHAR[morse_code] || 'Not a Morse code character!'
+end
+
+def decode_word(morse_word)
+  morse_word.strip.split.map { |char| decode_char(char) }.join
+end
+
+def decode(morse_mssg)
+  morse_mssg.strip.split('   ').map { |word| decode_word(word) }.join(' ')
+end
+
+puts decode('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
